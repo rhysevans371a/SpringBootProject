0d24b1d33f15591210a70b2e52b372bc0de64a19
rhysevans371a <rhys.evans371a@gmail.com>
2021-10-04 14:25:14 +0100

    Added JpaRepsitory support

--- src/main/java/com/qa/spring/repo/ProductRepository.java
+++ src/main/java/com/qa/spring/repo/ProductRepository.java
@@ -1,5 +1,13 @@
 package com.qa.spring.repo;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.qa.spring.domain.Product;
+
+/**
+ * @author Rhys Evans
+ *
+ */
+public interface ProductRepository extends JpaRepository <Product, Long>{
 
 }
