0310d5b85685e0a03ecc45cd6cf0e572e860e5c9
rhysevans371a <rhys.evans371a@gmail.com>
2021-10-04 18:27:39 +0100

    Create added

--- src/main/java/com/qa/spring/SpringBootProjectApplication.java
+++ src/main/java/com/qa/spring/SpringBootProjectApplication.java
@@ -1,13 +1,16 @@
 package com.qa.spring;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class SpringBootProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProjectApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/qa/spring/controller/ProductController.java
+++ src/main/java/com/qa/spring/controller/ProductController.java
@@ -1,5 +1,36 @@
 package com.qa.spring.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.spring.domain.Product;
+import com.qa.spring.services.ProductService;
+
+@RestController
+@RequestMapping("/Product")
 public class ProductController {
 
+	@Autowired
+	private ProductService service;
+
+	@GetMapping("/Read")
+	public ResponseEntity <List<Product>> readAll() {
+		return new ResponseEntity <List<Product>> (this.service.readAll(),HttpStatus.OK);
+	}
+	// create
+	@PostMapping("/create")
+	public ResponseEntity <Product> create(@RequestBody Product product) {
+		return new ResponseEntity <Product> (this.service.create(product),HttpStatus.CREATED);
+	}
 }
