8233ac6af99badebfacad9aa3c410992abe54a53
rhysevans371a <rhys.evans371a@gmail.com>
2021-10-04 14:44:47 +0100

    Create added

--- src/main/java/com/qa/spring/services/ProductService.java
+++ src/main/java/com/qa/spring/services/ProductService.java
@@ -1,5 +1,29 @@
 package com.qa.spring.services;
 
-public class ProductService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.spring.domain.Product;
+import com.qa.spring.repo.ProductRepository;
+
+/**
+ * @author Rhys Evans
+ *
+ */
+@Service
+public class ProductService {
+//	Create instance of repository
+	@Autowired
+	private ProductRepository repo;
+	
+//	Read All
+	public List<Product> listAll(){
+		return repo.findAll();
+	}
+//	Create
+	public void create(Product product) {
+		repo.save(product);
+	}
 }
