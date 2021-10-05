4d8433423ddf6360ba44f9207f49576a5b2e78ab
rhysevans371a <rhys.evans371a@gmail.com>
2021-10-05 11:00:25 +0100

    testEquals for class added

--- src/test/java/com/qa/spring/domain/ProductTest.java
+++ src/test/java/com/qa/spring/domain/ProductTest.java
@@ -1,5 +1,17 @@
 package com.qa.spring.domain;
 
-public class ProductTest {
 
+import org.junit.Test;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+
+public class ProductTest {
+	@Test
+	public void testEquals() {
+		EqualsVerifier.forClass(Product.class)
+		.suppress(Warning.NONFINAL_FIELDS)
+		.usingGetClass()
+		.verify();
+	}
 }
