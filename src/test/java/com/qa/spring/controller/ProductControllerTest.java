ece89a748b84a091ef0b1969d1c18a9c578f0af2
rhysevans371a <rhys.evans371a@gmail.com>
2021-10-05 16:18:18 +0100

    readAll test added

--- src/test/java/com/qa/spring/controller/ProductControllerTest.java
+++ src/test/java/com/qa/spring/controller/ProductControllerTest.java
@@ -1,5 +1,65 @@
 package com.qa.spring.controller;
 
-public class ProductControllerTest {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.spring.domain.Product;
+import com.qa.spring.services.ProductService;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+
+public class ProductControllerTest {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private ObjectMapper mapper;
+	
+	@MockBean
+	private ProductService service;
+	@Test
+	public void createTest() throws Exception {
+		Product product = new Product("Apple", "Fruit", "each", 0.50);
+		String productAsJSON = this.mapper.writeValueAsString(product);
+		
+		Mockito.when(this.service.create(product)).thenReturn(product);
+		
+		mvc.perform(post("/Product/create")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(productAsJSON))
+			.andExpect(status().isCreated())
+			.andExpect(content().json(productAsJSON));		
+	}
+	@Test
+	public void readAllTest() throws Exception {
+		Product product = new Product("Apple", "Fruit", "each", 0.50);
+		List<Product> output = new ArrayList<>();
+		output.add(product);
+		String outputAsJSON = this.mapper.writeValueAsString(output);
+		
+		Mockito.when(this.service.readAll()).thenReturn(output);
+		
+		mvc.perform(get("/Product/Read")
+			.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(content().json(outputAsJSON));
+	}
 }
