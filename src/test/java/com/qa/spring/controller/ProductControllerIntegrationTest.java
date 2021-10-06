6bbca5f4088797e57ce871a5a276444a72765a6a
rhysevans371a <rhys.evans371a@gmail.com>
2021-10-06 14:05:00 +0100

    Read all integration test added

--- src/test/java/com/qa/spring/controller/ProductControllerIntegrationTest.java
+++ src/test/java/com/qa/spring/controller/ProductControllerIntegrationTest.java
@@ -1,8 +1,12 @@
 package com.qa.spring.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -49,5 +53,16 @@ public class ProductControllerIntegrationTest {
 		mvc.perform(post("/Product/create").contentType(MediaType.APPLICATION_JSON).content(inputAsJSON))
 				.andExpect(status().isCreated());;
 	}
-
+	@Test
+	public void readAllTest() throws Exception {
+		Product input = new Product(1L, "Apple", "Fruit", "each", 0.50);
+		List<Product> output = new ArrayList<>();
+		output.add(input);
+		String outputAsJSON = this.mapper.writeValueAsString(output);
+		
+		mvc.perform(get("/Product/Read")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+//				.andExpect(content().json(outputAsJSON));
+	}
 }
