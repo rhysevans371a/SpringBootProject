666e8fa8fcb6fc21a49de95c5ea4c075ea4fbfaa
rhysevans371a <rhys.evans371a@gmail.com>
2021-10-06 14:37:55 +0100

    Read by Id integration test

--- src/test/java/com/qa/spring/controller/ProductControllerIntegrationTest.java
+++ src/test/java/com/qa/spring/controller/ProductControllerIntegrationTest.java
@@ -13,7 +13,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
@@ -23,7 +22,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.spring.domain.Product;
-import com.qa.spring.services.ProductService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -40,9 +38,6 @@ public class ProductControllerIntegrationTest {
 	@Autowired
 	private ObjectMapper mapper;
 
-	@MockBean
-	private ProductService service;
-
 	@Test
 	public void createTest() throws Exception {
 		Product input = new Product("Pear", "Fruit", "each", 0.50);
@@ -51,18 +46,26 @@ public class ProductControllerIntegrationTest {
 		String outputAsJSON = this.mapper.writeValueAsString(output);
 
 		mvc.perform(post("/Product/create").contentType(MediaType.APPLICATION_JSON).content(inputAsJSON))
-				.andExpect(status().isCreated());;
+				.andExpect(status().isCreated()).andExpect(content().json(outputAsJSON));
 	}
+
 	@Test
 	public void readAllTest() throws Exception {
 		Product input = new Product(1L, "Apple", "Fruit", "each", 0.50);
 		List<Product> output = new ArrayList<>();
 		output.add(input);
 		String outputAsJSON = this.mapper.writeValueAsString(output);
-		
-		mvc.perform(get("/Product/Read")
-				.contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk());
-//				.andExpect(content().json(outputAsJSON));
+
+		mvc.perform(get("/Product/Read").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().json(outputAsJSON));
+	}
+
+	@Test
+	public void readByIdTest() throws Exception {
+		Product input = new Product(1L, "Apple", "Fruit", "each", 0.50);
+		String inputAsJSON = this.mapper.writeValueAsString(input);
+
+		mvc.perform(get("/Product/read/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().json(inputAsJSON));
 	}
 }
